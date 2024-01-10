1c5974e265ffc82d541ae57cce443b2893cd956c
ariqmbrilian <ariqmbrilian@gmail.com>
2024-01-10 03:30:11 +0000

    Delete unnecessary folder & file

--- caddy/application/app.py
+++ caddy/application/app.py
@@ -1,9 +1,11 @@
 import time
+import os
 
 import redis
 from flask import Flask
 
 app = Flask(__name__)
+app_name = os.getenv('APP_NAME', 'default')
 cache = redis.Redis(host='redis', port=6379)
 
 def get_hit_count():
@@ -20,4 +22,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'app2 have been seen {} time.\n'.format(count)
+    return f'{app_name} been seen {count} time.\n'
