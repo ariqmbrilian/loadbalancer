6eb5bf55e3ed4707c774c5090d7cf573c6112410
ariqmbrilian <ariqmbrilian@gmail.com>
2024-05-22 23:24:19 +0700

    Adjustment

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,4 +50,16 @@ resource "azurerm_network_security_group" "lb-nsg" {
     destination_address_prefix = "*"
   }
 
+  security_rule {
+    name                       = "Cadvisor"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
