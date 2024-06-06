9173b6ed8e8faed8aa52dd2f899319e023472d10
ariqmbrilian <ariqmbrilian@gmail.com>
2024-06-06 09:27:04 +0700

    create bash

---
+++ script.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+set -o errexit
+
+CADDY_COMPOSE_FILE="caddy/docker-compose.yml"
+NGINX_COMPOSE_FILE="nginx/docker-compose.yml"
+
+
+create_vm() {
+  cd terraform
+  terraform init
+  terraform apply -auto-approve
+  cd ..
+}
+
+delete_vm() {
+  cd terraform
+  terraform destroy -auto-approve
+  rm terraform.state
+  cd ..
+}
+
+start_caddy() {
+  docker build -t application application/
+  docker compose -f $CADDY_COMPOSE_FILE up -d
+}
+
+stop_caddy() {
+  docker compose -f $CADDY_COMPOSE_FILE down --volumes
+}
+
+start_nginx() {
+  docker build -t application application/
+  docker compose -f $NGINX_COMPOSE_FILE up -d
+}
+
+stop_nginx() {
+  docker compose -f $NGINX_COMPOSE_FILE down --volumes
+}
+
+case "$1" in
+  create-vm)
+    create_vm
+    ;;
+  delete-vm)
+    delete_vm
+    ;;
+  start-caddy)
+    start_caddy
+    ;;
+  stop-caddy)
+    stop_caddy
+    ;;
+  start-nginx)
+    start_nginx
+    ;;
+  stop-nginx)
+    stop_nginx
+    ;;
+  clean)
+    clean
+    ;;
+  *)
+    echo "Usage: $0 {start-caddy|stop-caddy|start-nginx|stop-nginx|clean}"
+    exit 1
+    ;;
+esac
